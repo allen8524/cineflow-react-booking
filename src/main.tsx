7d57f9cc522b58016fb3c8f58af5bf8955fd34d1
황민서 <102010606+allen8524@users.noreply.github.com>
2026-05-30 16:20:18 +0900

    style: 영화 카드 메타 패널 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/home.css';
 import './styles/movie-list.css';
 import './styles/admin.css';
 import './styles/auth.css';
+import './styles/movie-card.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
