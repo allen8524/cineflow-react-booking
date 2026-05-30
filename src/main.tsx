43a4a70d7437197f3cb70befc0049d191e2219fa
황민서 <102010606+allen8524@users.noreply.github.com>
2026-05-30 14:05:38 +0900

    style: 영화 목록 페이지네이션 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './styles/complete.css';
 import './styles/home.css';
+import './styles/movie-list.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
